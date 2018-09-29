34ee1bdbdb0461b0414c4c9cd741ccaa79193c52
Lauren Hetherington <hetherington@astro.utoronto.ca>
2018-09-29 15:48:54 -0400

    More detail on team-selection. Input fields exist. Don't do anything yet.

---
+++ Assets/Scripts/CharSelect.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharSelect : MonoBehaviour {
+    public InputField Name;
+    public InputField Pronouns;
+    public Dropdown Classes;
+    public Text infotext;
+    //string[]
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
