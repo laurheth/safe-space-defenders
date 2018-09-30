8e825ed2ef3138df531d5846971debf4f2cb21b6
Lauren Hetherington <hetherington@astro.utoronto.ca>
2018-09-30 17:34:04 -0400

    Started code for character selection.

--- Assets/Scripts/CharSelect.cs
+++ Assets/Scripts/CharSelect.cs
@@ -4,14 +4,26 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class CharSelect : MonoBehaviour {
-    public InputField Name;
-    public InputField Pronouns;
-    public Dropdown Classes;
-    public Text infotext;
+    GameObject NameInputObj;
+    GameObject PronounInputObj;
+    GameObject ClassInputObj;
+    GameObject InfoTextObj;
+    InputField Name;
+    InputField Pronouns;
+    Dropdown Classes;
+    Text infotext;
     //string[]
 	// Use this for initialization
 	void Start () {
-		
+        NameInputObj = transform.Find("Details/InputFieldName").gameObject;
+        PronounInputObj = transform.Find("Details/InputPronouns").gameObject;
+        ClassInputObj = transform.Find("Details/InputClass").gameObject;
+        InfoTextObj = transform.Find("InfoText").gameObject;
+
+        Name = NameInputObj.GetComponent<InputField>();
+        Pronouns = PronounInputObj.GetComponent<InputField>();
+        Classes = ClassInputObj.GetComponent<Dropdown>();
+        infotext = InfoTextObj.GetComponent<Text>();
 	}
 	
 	// Update is called once per frame
